c1746a56c252811963404e4aff5b35de40702ce2
joahubi <jonathanaboh3@gmail.com>
2026-09-10 16:33:28 +0100

    project

--- business-manager/main.go
+++ business-manager/main.go
@@ -17,77 +17,95 @@ func main() {
 
 	var choice int
 
-	fmt.Println()
-	fmt.Println("1. Add Product")
-	fmt.Println("2. View Products")
-	fmt.Println("3. Record Sale")
-	fmt.Println("4. View Sales")
-	fmt.Println("5. Exit")
-	fmt.Println()
+	for {
+		fmt.Println()
+		fmt.Println("1. Add Product")
+		fmt.Println("2. View Products")
+		fmt.Println("3. Record Sale")
+		fmt.Println("4. View Sales")
+		fmt.Println("5. Exit")
+		fmt.Println()
 
-	fmt.Print("Choose an option: ")
+		fmt.Print("Choose an option: ")
+		fmt.Scanln(&choice)
 
-	fmt.Scanln(&choice)
+		switch choice {
 
-	switch choice {
+		case 1:
+			var productName string
 
-	case 1:
-		var productName string
+			fmt.Print("Enter product name: ")
+			fmt.Scanln(&productName)
 
-		fmt.Print("Enter product name: ")
-		fmt.Scanln(&productName)
+			fmt.Println("Product name:", productName)
 
-		fmt.Println("Product name:", productName)
+			var productPrice float64
 
-		var productPrice float64
+			for {
+				fmt.Print("Enter product price: ")
 
-		for {
-			fmt.Print("Enter product price: ")
-
-			if _, err := fmt.Scanln(&productPrice); err != nil {
-				fmt.Println("Invalid price")
-			} else {
-				break
-			}
-		}
-
-		fmt.Println("Product price:", productPrice)
-
-		var productQuantity int
-		for {
-			fmt.Print("Enter product quantity: ")
-			for i := 0; i >= 0; i++ {
-				if _, err := fmt.Scanln(&productQuantity); err != nil {
-					fmt.Println("Invalid Quantity:")
+				if _, err := fmt.Scanln(&productPrice); err != nil {
+					fmt.Println("Invalid price")
+				} else {
+					break
 				}
-				break
-		    }
+			}
+
+			fmt.Println("Product price:", productPrice)
+
+			var productQuantity int
+
+			for {
+				fmt.Print("Enter product quantity: ")
+
+				if _, err := fmt.Scanln(&productQuantity); err != nil {
+					fmt.Println("Invalid quantity")
+				} else if productQuantity <= 0 {
+					fmt.Println("Quantity must be greater than 0")
+				} else {
+					break
+				}
+			}
+
+			fmt.Println("Product quantity:", productQuantity)
+
+			product := Product{
+				Name:     productName,
+				Price:    productPrice,
+				Quantity: productQuantity,
+			}
+
+			products = append(products, product)
+
+			fmt.Println("Product added:", product)
+
+		case 2:
+			fmt.Println()
+			fmt.Println("===== PRODUCTS =====")
+
+			if len(products) == 0 {
+		        fmt.Println("No products available.")
+            }
 			
-			
+			for i, product := range products {
+				fmt.Println("Product", i+1)
+				fmt.Println("Name:", product.Name)
+				fmt.Println("Price:", product.Price)
+				fmt.Println("Quantity:", product.Quantity)
+				fmt.Println()
+			}
+		case 3:
+			fmt.Println("Record Sale selected")
+
+		case 4:
+			fmt.Println("View Sales selected")
+
+		case 5:
+			fmt.Println("Goodbye!")
+			return
+
+		default:
+			fmt.Println("Invalid option")
 		}
-		fmt.Println("Product quantity:", productQuantity)
-
-		product := Product{
-			Name:     productName,
-			Price:    productPrice,
-			Quantity: productQuantity,
-		}
-
-		fmt.Println("Product added:", product)
-
-	case 2:
-		fmt.Println("View Products selected")
-
-	case 3:
-		fmt.Println("Record Sale selected")
-
-	case 4:
-		fmt.Println("View Sales selected")
-
-	case 5:
-		fmt.Println("Goodbye!")
-
-	default:
-		fmt.Println("Invalid option")
 	}
 }
